d046bf6b9b27a8618a244dc597f10990366fd6b1
Gavin Samuels <gsamuels@fandango.com>
2018-05-03 08:41:33 -0700

    add project linting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "mocha": true
+    },
+    "rules": {
+        "no-underscore-dangle": 0,
+        "indent": ["warn", 2],
+        "class-methods-use-this": 0,
+        "arrow-parens":["as-needed"]
+    },
+
+};
